1cd3e3a4c79cc54aed543033e2823f83b94a8938
kwangmoyang <zjqtm45@naver.com>
2024-03-04 00:18:53 +0900

    feat : MongoDB Settings

--- Nodejs_workspace/index.js
+++ Nodejs_workspace/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://zjqtm45:9kogs8AlaKX81bDY@cluster0.snir30v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
 app.get('/', (req,res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
