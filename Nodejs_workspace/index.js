835bc4ea0ed36161b258d1b46ce4cd90648004fe
kwangmoyang <zjqtm45@naver.com>
2024-03-11 23:20:58 +0900

    feat : index.js

--- Nodejs_workspace/index.js
+++ Nodejs_workspace/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } =require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+
+//application/ json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -10,5 +19,22 @@ mongoose.connect('', {
 
 
 app.get('/', (req,res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    //会員加入の時必要な情報をクライアントから持ってくると
+    //それをデータベースに入れる
+    
+    
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+          success : true
+      })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
