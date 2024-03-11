b0224166885ed59e0c2c0ceb303eeb6d79c92246
kwangmoyang <zjqtm45@naver.com>
2024-03-11 23:47:20 +0900

    feat : MongoDB Version Update

--- Nodejs_workspace/index.js
+++ Nodejs_workspace/index.js
@@ -21,20 +21,27 @@ mongoose.connect('', {
 app.get('/', (req,res) => res.send('Hello World!'))
 
 
-app.post('/register', (req, res) => {
-    //会員加入の時必要な情報をクライアントから持ってくると
-    //それをデータベースに入れる
+app.post('/register', async(req, res) => {
     
-    
+    try {
+      //会員加入の時必要な情報をクライアントから持ってくると
+      //それをデータベースに入れる
 
-    const user = new User(req.body)
+      const user = new User(req.body);
 
-    user.save((err, userInfo) => {
-        if(err) return res.json({ success: false, err})
-        return res.status(200).json({
-          success : true
-      })
-    })
+      const savedUser = await user.save();
+      return res.status(200).json({
+        success: true,
+        userInfo: savedUser
+      });
+    } catch (err) {
+        console.error(err);
+        return res.json({success: false, err});
+    }
+
+  
+
+   
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
